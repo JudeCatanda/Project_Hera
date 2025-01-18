7d90f5f74e4bedb4c337200380a6caf7f3bd9123
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-01-18 21:32:35 +0800

    add draw call

--- src/mesh.c
+++ src/mesh.c
@@ -23,6 +23,19 @@ void mesh_proc_create(Mesh* msh, Vertex* in_vertices) {
   vao->unbind(vao);
 }
 
+  // void(*draw)(struct Mesh* msh);
+void mesh_proc_draw(Mesh* msh) {
+  Layout* vao = &msh->vao;
+  ShaderProgram* program = &msh->program;
+
+  program->use_program(program);
+  vao->bind(vao);
+  glDrawArrays(GL_TRIANGLES, 0, (unsigned int)(sizeof(msh->vertices) / sizeof(Vertex))); //idk if its right but ok
+  vao->unbind(vao);
+  program->unbind(program);
+};
+
 void mesh_init(Mesh *mesh) {
   mesh->create = mesh_proc_create;
+  mesh->draw = mesh_proc_draw;
 }
