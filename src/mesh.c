27cffb853fa22421083a03231bc0345b9bd13cef
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-01-18 21:44:42 +0800

    ok

--- src/mesh.c
+++ src/mesh.c
@@ -17,7 +17,7 @@ void mesh_proc_create(Mesh* msh, Vertex* in_vertices) {
   layout_init(vao);
   vao->create_and_bind(vao);
 
-  buffer_create(vbo, msh->buffer_size, &msh->vertices[0], GL_STATIC_DRAW, GL_ARRAY_BUFFER);
+  buffer_create(vbo, msh->buffer_size, msh->vertices, GL_STATIC_DRAW, GL_ARRAY_BUFFER);
   layout_enable_and_set_vertex_attrib_pointer(0, 2, GL_FLOAT, sizeof(Vertex), (const void*)offsetof(Vertex, position));
   layout_enable_and_set_vertex_attrib_pointer(1, 3, GL_FLOAT, sizeof(Vertex), (const void*)offsetof(Vertex, color));
   vao->unbind(vao);
