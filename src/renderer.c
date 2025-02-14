ce8cc00a5abd55142cbd2976f8e3d80473d7b666
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-02-14 18:04:43 +0800

    cleaned the code base

--- src/renderer.c
+++ src/renderer.c
@@ -24,14 +24,7 @@ void Init(Renderer *data) {
   Buffer* ebo = &data->ebo;
   Shader *vertex = &data->vertex, *fragment = &data->fragment;
   ShaderProgram* program = &data->shdr_program;
-  // mesh_init(quad);
 
-  // Vertex vertices[3] = {
-  //   (Vertex){ -0.5, -0.5, 1.0, 0.0, 0.0 }, 
-  //   (Vertex){ -0.5,  0.5, 0.0, 1.0, 0.0 },
-  //   (Vertex){  0.5, -0.5, 0.0, 0.0, 1.0 }
-  // };
-  // shader_create(vertex, "./assets/test_build/main.vert", GL_VERTEX_SHADER);
   shader_create(vertex, GET_TEST_BUILD_PATH("main.vert"), GL_VERTEX_SHADER);
   shader_create(fragment, GET_TEST_BUILD_PATH("main.frag"), GL_FRAGMENT_SHADER);
   program_create(program, vertex, fragment);
@@ -47,13 +40,6 @@ void Init(Renderer *data) {
   buffer_create(ebo, BATCH_INDEX_BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW, GL_ELEMENT_ARRAY_BUFFER);
   layout_enable_and_set_vertex_attrib_pointer(0, 2, GL_FLOAT, sizeof(Vertex), (const void*)offsetof(Vertex, Position));
   vao->unbind(vao);
-  // quad->create(quad, vertices);
-
-  data->player_movement_speed = 0.001f;
-  data->player_velocity = (vec2s) {
-    .x = 0.0f,
-    .y = 0.0f
-  };
 }
 
 void Update(Renderer *data) {
@@ -63,30 +49,6 @@ void Update(Renderer *data) {
   Buffer* ebo = &data->ebo;
   Buffer* a_buffer_in_which_we_store_positions = &data->a_buffer_in_which_we_store_positions;
   ShaderProgram* program = &data->shdr_program;
-  // Mesh* quad = &data->quad;
-
-  Vertex vertices[BATCH_TOTAL_VERTEX_COUNT];
-  Vertex* pVertices = vertices;
-
-  data->player_pos = (vec2s){ .x = 0.0f, .y = 0.0f };
-
-  pVertices = vertex_create(pVertices, data->player_pos.x, data->player_pos.y, 0.1f);
-  pVertices = vertex_create(pVertices, -0.2f, 0.0f, 0.1f);
-  pVertices = vertex_create(pVertices, 0.0f, -0.2f, 0.1f);
-
-  unsigned int ebo_a[BATCH_TOTAL_INDEX_COUNT];
-  unsigned int* pEbo = ebo_a;
-
-  for(int i = 0; i < BATCH_RENDER_QUAD_COUNT; i++) {
-    pEbo = ebo_add_index(pEbo, i * 4);
-  }
-
-  vec2s positions[BATCH_RENDER_QUAD_COUNT];
-  init_vec2s_array(positions, BATCH_RENDER_QUAD_COUNT, 0.0f, 0.0f);
-
-  const float target_color = 1.0f;
-  float start_color = 0.0f;
-  float get_val = 0.0;
 
   window->get_size(window);
   LOG_WARNING("The overall size: %f", (float)data->window->size.y / (float)data->window->size.y);
@@ -95,8 +57,6 @@ void Update(Renderer *data) {
   LOG_DEBUG("time is %f", (float)glfwGetTime());
   window->update_aspect_ratio(window);
   LOG_DEBUG("aspect ratio is %f", window->aspect_ratio);
-
-  const float PLAYER_SPEED = 0.3f;
 
   while(!window->should_close(window)) {
 
@@ -109,50 +69,10 @@ void Update(Renderer *data) {
 
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.2, 0.5, 0.9, 1.0);
-
-    // quad->vertex_count = 6;
-    // quad->bind_all(quad);
-    // float lerp_result = lerp(start_color, target_color, 0.4 * data->delta_time);
-    uniform_send_float_once(program->handle, "u_time", 1, data->current_time);
-    // uniform_send_float_once(quad->program.handle, "lerp_value", 1, lerp_result)
-    // start_color = lerp_result;
-    // quad->draw_call(quad);
-    // quad->unbind_all(quad);
-
-    buffer_setdata(vbo, 0, BATCH_VERTEX_BUFFER_SIZE, &vertices);
-    buffer_setdata(ebo, 0, BATCH_INDEX_BUFFER_SIZE, &ebo_a);
-    buffer_setdata(a_buffer_in_which_we_store_positions, 0, BATCH_RENDER_QUAD_COUNT* sizeof(vec2s), &positions);
-
-    program->use_program(program);
-    vao->bind(vao);
-
-    positions[0].x += data->player_velocity.x * data->delta_time;
-    positions[1].x -= data->player_velocity.x * data->delta_time;
-    if(window->is_key_pressed(window, GLFW_KEY_D)) {
-      data->player_velocity.x += data->player_movement_speed;
-      // LOG_DEBUG("key was pressed");
-    }
-    if(window->is_key_pressed(window, GLFW_KEY_A)) {
-      data->player_velocity.x -= data->player_movement_speed;
-      // LOG_DEBUG("key was pressed");
-    }
-
-    // glDrawArrays(GL_TRIANGLES, 0, BATCH_TOTAL_VERTEX_COUNT); //idk maygbe works?
-    // glDrawElements(GL_TRIANGLES, BATCH_TOTAL_INDEX_COUNT, GL_UNSIGNED_INT, 0);
-    glDrawElementsInstanced(GL_TRIANGLES, BATCH_TOTAL_INDEX_COUNT, GL_UNSIGNED_INT, 0, BATCH_RENDER_QUAD_COUNT);
-    vao->unbind(vao);
-
     if(glfwGetKey(window->handle, GLFW_KEY_ESCAPE) == GLFW_PRESS)
       break;
     if(glfwGetKey(window->handle, GLFW_KEY_F3) == GLFW_PRESS)
       glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    if(window->is_key_pressed(window, GLFW_KEY_F10)) {
-      data->player_velocity = (vec2s) {
-        .x = 0.0f,
-        .y = 0.0f
-      };
-      init_vec2s_array(positions, BATCH_RENDER_QUAD_COUNT, 0.0f, 0.0f);
-    }
 
     glfwSwapBuffers(window->handle);
     glfwPollEvents();
