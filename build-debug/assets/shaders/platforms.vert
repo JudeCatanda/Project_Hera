125a82d31ee4d04e42ee863b9ec5c278794840e4
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-02-17 20:21:39 +0800

    cam movement not working

--- build-debug/assets/shaders/platforms.vert
+++ build-debug/assets/shaders/platforms.vert
@@ -4,7 +4,8 @@ layout (location = 0) in vec2 local_mesh_postions;
 layout (location = 1) in vec2 offset_positions;
 
 uniform mat4 projection;
+uniform mat4 view;
 
 void main() {
-  gl_Position = projection * vec4(local_mesh_postions + offset_positions, 0.0, 1.0);
+  gl_Position = projection * view * vec4(local_mesh_postions + offset_positions, 0.0, 1.0);
 };
--- src/camera.c
+++ src/camera.c
@@ -3,17 +3,16 @@
 #undef LOG_DEBUG 
 #define LOG_DEBUG(fmt, ...) printf("[DEBUG] " fmt "\n", ##__VA_ARGS__)
 
-// void(*send_matrices)(struct Camera* cam, ShaderProgram* program);
-void proc_camera_send_matrices(Camera* cam, ShaderProgram* program) {
+void proc_camera_send_matrices(Camera* cam, ShaderProgram* program) { //functions gets called every frame
   Window* window = cam->window;
   window->update_aspect_ratio(window);
   window->get_size(window);
   cam->orthro = glms_ortho(-window->aspect_ratio, window->aspect_ratio, -2.0f, 1.0f, -1.0f, 1.0f);
   cam->view = glms_mat4_identity();
-  cam->view = glms_translate(cam->view, (vec3s){ .x = cam->camera_position.x, .y = cam->camera_position.y, .z = 0.0f });
+  cam->view = glms_translate(cam->view, (vec3s){ .x = cam->camera_position.x, .y = cam->camera_position.y, .z = 0.0f }); //move the camera
+
   glUniformMatrix4fv(uniform_location(program->handle, "projection"), 1, GL_FALSE, (const float*)&cam->orthro.raw);
   glUniformMatrix4fv(uniform_location(program->handle, "view"), 1, GL_FALSE, (const float*)&cam->view.raw[0][0]);
-  // LOG_DEBUG("window is not null.. here have its size! %dx%d", window->size.x, window->size.y);// actually prints the size! DYNAMICALY!
 };
 void camera_init(Camera *cam) {
   cam->update = proc_camera_send_matrices;
