a589a02074c5d0f87e8d4a4d49bbcc9a56711539
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-02-17 17:21:45 +0800

    alr

--- src/camera.c
+++ src/camera.c
@@ -10,9 +10,9 @@ void proc_camera_send_matrices(Camera* cam, ShaderProgram* program) {
   window->get_size(window);
   cam->orthro = glms_ortho(-window->aspect_ratio, window->aspect_ratio, -2.0f, 1.0f, -1.0f, 1.0f);
   cam->view = glms_mat4_identity();
-  glUniformMatrix3fv(uniform_location(program->handle, "projection"), 1, GL_FALSE, (const float*)&cam->orthro.raw);
-  glUniformMatrix3fv(uniform_location(program->handle, "view"), 1, GL_FALSE, (const float*)&cam->view.raw[0][0]);
-  LOG_DEBUG("window is not null.. here have its size! %dx%d", window->size.x, window->size.y);
+  glUniformMatrix4fv(uniform_location(program->handle, "projection"), 1, GL_FALSE, (const float*)&cam->orthro.raw);
+  glUniformMatrix4fv(uniform_location(program->handle, "view"), 1, GL_FALSE, (const float*)&cam->view.raw[0][0]);
+  // LOG_DEBUG("window is not null.. here have its size! %dx%d", window->size.x, window->size.y);// actually prints the size! DYNAMICALY!
 };
 void camera_init(Camera *cam) {
   cam->update = proc_camera_send_matrices;
