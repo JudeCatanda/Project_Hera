9df71592ac5a5391344976c7496771b901537776
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-07-29 18:31:39 +0800

    Rename Shader file classes

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -1,109 +1,94 @@
 #include "Shader.hpp"
 
-void Shader::read_file(std::string file_name) {
-  std::string shader_souce;
-  std::ifstream shader_file;
-  shader_file.open(file_name);
-
-  if(!shader_file.is_open()) {
-    std::fprintf(stderr, "[ERROR] file not found: %s\n", file_name.c_str());
-    return;
-  }
-  
-  std::stringstream shader_stream;
-  shader_stream << shader_file.rdbuf();
-
-  shader_file.close();
-
-  shader_souce = shader_stream.str();
-  this->source_code = shader_souce; //bad?????????
+void CShader::readFile(std::string fileName) {
+    std::string shader_souce;
+    std::ifstream shader_file;
+    shader_file.open(fileName);
+    
+    if(!shader_file.is_open()) {
+        std::fprintf(stderr, "[ERROR] file not found: %s\n", fileName.c_str());
+        return;
+    }
+    
+    std::stringstream shader_stream;
+    shader_stream << shader_file.rdbuf();
+    
+    shader_file.close();
+    
+    shader_souce = shader_stream.str();
+    m_szShaderSourceCode =  shader_souce; //bad??
 }
 
-void Shader::create(std::string file_name, GLenum type) {
-  this->read_file(file_name);
-  const char* data = this->source_code.c_str(); // also bad???
-  this->m_Handle = glCreateShader(type);
-  glShaderSource(this->m_Handle, 1, &data, NULL);
-  glCompileShader(this->m_Handle);
-  this->type = type;
-  this->check_errors();
+void CShader::Create(std::string szFileName, GLenum nType) {
+        readFile(szFileName);
+        const char* data = m_szShaderSourceCode.c_str(); // also bad???
+        this->m_nHandle = glCreateShader(nType);
+        glShaderSource(this->m_nHandle, 1, &data, nullptr);
+        glCompileShader(this->m_nHandle);
+        this->m_nShaderType = nType;
+        CheckErrors();
 }
 
-GLenum Shader::get_type() {
-  return this->type;
+GLenum CShader::GetShaderType() const  {
+    return m_nShaderType; 
 }
 
-unsigned int Shader::GetHandle() {
-  return this->m_Handle;
+unsigned int CShader::GetHandle() const {
+    return m_nHandle;
 }
 
-void Shader::check_errors() {
-  int success;
-  char err_out[1024];
-  glGetShaderiv(this->m_Handle, GL_COMPILE_STATUS, &success);
-  if(!success) {
-    glGetShaderInfoLog(this->m_Handle, 1024, NULL, err_out);
-    std::fprintf(stderr, "[SHADER ERROR] %s\n", err_out);
-  }
+void CShader::CheckErrors() {
+    int nSuccess;
+    char szErrorBuffer[1024];
+    glGetShaderiv(this->m_nHandle, GL_COMPILE_STATUS, &nSuccess);
+    if(!nSuccess) {
+        glGetShaderInfoLog(this->m_nHandle, 1024, nullptr, szErrorBuffer);
+        std::fprintf(stderr, "[SHADER ERROR] %s\n", szErrorBuffer);
+    }
 }
 
-void Shader::destroy() {
-  glDeleteShader(this->m_Handle);
+void CShader::Destroy() {
+    glDeleteShader(m_nHandle);
 }
 
-void ShaderProgram::CreateProgram(Shader* vertex, Shader* fragment) {
-  this->m_Handle = glCreateProgram();
-  glAttachShader(this->m_Handle, vertex->GetHandle());
-  glAttachShader(this->m_Handle, fragment->GetHandle());
-  glLinkProgram(this->m_Handle);
-  this->CheckErrors();
-  vertex->destroy();
-  fragment->destroy();
-  this->BindProgram(); //basically use_program!
+void CShaderProgram::CreateProgram(CShader* pVertex, CShader* pFragment) {
+    m_nHandle = glCreateProgram();
+    glAttachShader(m_nHandle, pVertex->GetHandle());
+    glAttachShader(m_nHandle, pFragment->GetHandle());
+    glLinkProgram(m_nHandle);
+    CheckErrors();
+    pVertex->Destroy();
+    pFragment->Destroy();
+    BindProgram();
 }
 
-void ShaderProgram::BindProgram() {
-  glUseProgram(this->m_Handle);
+void CShaderProgram::BindProgram() {
+    glUseProgram(m_nHandle);
 }
 
-void ShaderProgram::UnbindProgram() {
-  glUseProgram(0);
+void CShaderProgram::UnbindProgram() {
+    glUseProgram(0);
 }
 
-unsigned int ShaderProgram::GetHandle() {
-  return this->m_Handle;
+unsigned int CShaderProgram::GetHandle() const {
+    return m_nHandle;
 }
 
-void ShaderProgram::CheckErrors() {
-  int success;
-  char err_out[1024];
-  glGetShaderiv(this->m_Handle, GL_LINK_STATUS, &success);
-  if(!success) {
-    glGetShaderInfoLog(this->m_Handle, 1024, NULL, err_out);
-    std::fprintf(stderr, "[SHADER PROGRAM ERROR] %s\n", err_out);
-  }
+void CShaderProgram::CheckErrors() {
+    int nSuccess;
+    char szErrorBuffer[1024];
+    glGetShaderiv(this->m_nHandle, GL_COMPILE_STATUS, &nSuccess);
+    if(!nSuccess) {
+        glGetShaderInfoLog(this->m_nHandle, 1024, nullptr, szErrorBuffer);
+        std::fprintf(stderr, "[SHADER ERROR] %s\n", szErrorBuffer);
+    }
 }
 
-void ShaderProgram::Destroy() {
-  this->UnbindProgram();
-  glDeleteProgram(this->m_Handle);
+void CShaderProgram::Destroy() {
+    UnbindProgram();
+    glDeleteProgram(m_nHandle);
 }
 
-void ShaderProgram::send_uniform_float(std::string uniform, float data) {
-  glUniform1f(glGetUniformLocation(this->m_Handle, uniform.c_str()), data);
-}
-void ShaderProgram::send_uniform_float2(std::string uniform, float x, float y) {
-  glUniform2f(glGetUniformLocation(this->m_Handle, uniform.c_str()), x, y);
-}
-
-void ShaderProgram::send_uniform_float3(std::string uniform, float x, float y, float z) {
-  glUniform3f(glGetUniformLocation(this->m_Handle, uniform.c_str()), x, y, z);
-}
-
-void ShaderProgram::send_uniform_float4(std::string uniform, float x, float y, float z, float w) {
-  glUniform4f(glGetUniformLocation(this->m_Handle, uniform.c_str()), x, y, z, w);
-}
-
-void ShaderProgram::CreateUniformBlock(const std::string& block_name) {
+void CShaderProgram::CreateUniformBlock(const std::string& szBlockName) {
 
 };
--- src/Shader.hpp
+++ src/Shader.hpp
@@ -5,38 +5,34 @@
 #include <glad/glad.h>
 
 class CShader {
-private:
-  unsigned int m_Handle;
-  GLenum m_ShaderType;
-  std::string m_ShaderSourceCode;
-  void readFile(std::string fileName);
-public:
-  CShader() = default;
-  void Create(std::string fileName, GLenum type);
-  GLenum Type() const;
-  unsigned int Handle() const;
-  void CheckErrors();
-  void Destroy();
+    private:
+    unsigned int m_nHandle;
+    GLenum m_nShaderType;
+    std::string m_szShaderSourceCode;
+    void readFile(std::string szFileName);
+
+    public:
+    CShader() = default;
+    void                               Create(std::string szFileName, GLenum nType);
+    GLenum                             GetShaderType() const;
+    unsigned int                       GetHandle() const;
+    void                               CheckErrors();
+    void                               Destroy();
 };
 
 class CShaderProgram {
-private:
-  unsigned int m_Handle;
-  unsigned int m_BlockIndex;
-public:
-  CShaderProgram() = default;
-  void CreateProgram(CShader* vertex, CShader* fragment);
-  /// @brief its just use program
-  void BindProgram();
-  void UnbindProgram();
-  unsigned int Handle() const;
-  void CheckErrors();
-  void Destroy();
+    private:
+    unsigned int m_nHandle;
+    unsigned int m_nBlockIndex;
 
-  void CreateUniformBlock(const std::string& block_name);
+    public:
+    CShaderProgram() = default;
+    void                               CreateProgram(CShader* pVertex, CShader* pFragment);
+    void                               BindProgram();
+    void                               UnbindProgram();
+    unsigned int                       GetHandle() const;
+    void                               CheckErrors();
+    void                               Destroy();
 
-  void send_uniform_float(std::string uniform, float data);
-  void send_uniform_float2(std::string uniform, float x, float y);
-  void send_uniform_float3(std::string uniform, float x, float y, float z);
-  void send_uniform_float4(std::string uniform, float x, float y, float z, float w);
+    void                               CreateUniformBlock(const std::string& szBlockName);
 };
