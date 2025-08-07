66a4ad347ff9b6acb59c61dcd0bae8e5f11a7787
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-08-07 19:51:32 +0800

    Rendering quads

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -19,9 +19,6 @@ const unsigned int indices_per_quad = 6;
 glm::vec2 cell_size;
 
 void Terrain::create() {
-  def_as_ptr(indices_buffer);
-  def_as_ptr(positions_buffer);
-  def_as_ptr(texture_positions_buffer);
 
   m_Reader.ReadMap(GET_PATH_FROM_MAPS_DIR("level0")"//test.map");
   render_count = m_Reader.GetLineCount();
@@ -50,166 +47,49 @@ void Terrain::create() {
   // mesh_buffer->Create((render_count * points_per_quad) * sizeof(glm::vec2), nullptr, GL_DYNAMIC_DRAW, GL_ARRAY_BUFFER);
   // vao->SetVertexAttrib(0, 2, GL_FLOAT, sizeof(glm::vec2), (const void *)0);
   
-  const float flTileSize = m_Reader.TileSize(); //get the default tile size in CBaseMapReader
+  const float kflTileSize = m_Reader.GetTileSize();
   m_MeshData = {
-      { .Position = glm::vec2(-flTileSize, -flTileSize) },
-      { .Position = glm::vec2( flTileSize, -flTileSize) },
-      { .Position = glm::vec2( flTileSize,  flTileSize) },
-      { .Position = glm::vec2( flTileSize,  flTileSize) },
-      { .Position = glm::vec2(-flTileSize,  flTileSize) },
-      { .Position = glm::vec2(-flTileSize, -flTileSize) }
+    -kflTileSize, -kflTileSize,
+     kflTileSize, -kflTileSize,
+     kflTileSize,  kflTileSize,
+
+     kflTileSize,  kflTileSize,
+    -kflTileSize,  kflTileSize,
+    -kflTileSize, -kflTileSize
   };
   m_VertexBuffer.Create(m_MeshData.size(), m_MeshData.data(), GL_STATIC_DRAW, GL_ARRAY_BUFFER);
   m_VertexArray.SetVertexAttrib(0, 2, GL_FLOAT, 2 * sizeof(float), (const void*)0);
   
 
-  positions_buffer->Unbind();
-  indices_buffer->Unbind();
   m_VertexBuffer.Unbind();
   m_VertexArray.Unbind();
 
-  this->tg.clear();
-  int idx = 0;
-  const float space = 4.0f;
-  const float x_starter = 0.0f;
-  glm::vec2 offset = glm::vec2(x_starter, 0.0f);
-
-  int nIterationCount = 0;
-
-  int sprite_x = 0;
-  int sprite_y = 0;
-
-  for (glm::vec2& QuadPos : *m_Reader.GetBuffer()) {
-    tg.push_quad_with_sprite(space, QuadPos, glm::vec2(32.0f), cell_size, glm::vec2(0.0f));
-    LOG_DEBUG("Positions: %.2f , %.2f", QuadPos.x, QuadPos.y);
-  }
-
-  // for(int y = 0; y < max_render_for_y; y++) {
-  //   for (int x = 0; x < max_render_for_x; x++) {
-  //     int rand_pos = dist(engine);
-  //     sprite_x = rand_pos % 2;
-  //     sprite_y = rand_pos / 2;
-  //     this->tg.push_quad_with_sprite(space, offset, glm::vec2(32.0f), cell_size, glm::vec2((float)sprite_x, (float)sprite_y)); //get randome texture
-  //     // LOG_DEBUG("%f, %f @ Quad", offset.x, offset.y);
-  //     if(nIterationCount < 4) {
-  //       LOG_DEBUG("%4.4f, %.4f @ QUAD_POS", offset.x, offset.y);
-  //     }
-  //     nIterationCount += 1;
-  //     idx+=1;
-  //     offset.x += space * 2;
-  //   }
-  //   offset.x = x_starter;
-  //   offset.y += space * 2;
-  // }
-
-  this->tg.gen_indices(m_Reader.GetLineCount());
-  this->texture_positions_buffer.UpdateData(0, this->tg.get_vector_with_sprite().size() * sizeof(glm::vec2), this->tg.get_with_sprite());
-  this->indices_buffer.UpdateData(0, this->tg.get_vector_with_indices_buffer().size(), this->tg.get_with_indices_buffer());
-
-  LOG_DEBUG("We are rendering %d quads", this->tg.get_rendered_quads());
 }
 
 void Terrain::draw() {
-  def_as_ptr(indices_buffer);
-  def_as_ptr(positions_buffer);
-
   m_ShaderProgram.BindProgram();
   this->atlas.bind_and_set_active(GL_TEXTURE1);
   m_VertexArray.Bind();
-  // indices_buffer->Bind();
-  // positions_buffer->Bind();
 
   this->hitbox.size = this->size; //fix this later
 
   //glDrawArrays(GL_TRIANGLES, 0, (max_vertex_counts * points_per_quad)); //if not using indices
   //glDrawElements(GL_TRIANGLES, (render_count * points_per_quad), GL_UNSIGNED_INT, nullptr);
-  glDrawArrays(GL_TRIANGLES, 0, render_count * points_per_quad);
+  glDrawArrays(GL_TRIANGLES, 0, render_count);
   
   m_ShaderProgram.UnbindProgram();
   m_VertexArray.Unbind();
-  indices_buffer->Unbind();
 }
 
 void Terrain::destroy() {
-  def_as_ptr(indices_buffer);
-  def_as_ptr(positions_buffer);
-
   m_ShaderProgram.Destroy();
   m_VertexArray.Destroy();
   m_VertexBuffer.Destroy();
-  indices_buffer->Destroy();
-  positions_buffer->Destroy();
 }
 
 void Terrain::test_tg(void) {
   //for testing
 }
-
-//bellow are depreacted functions (batch rendering)
-void Terrain_Generator::push_quad(float size, glm::vec2 pos) {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-void Terrain_Generator::update_quad(unsigned int base, glm::vec2 pos, float size) {
-  LOG_ERROR("NO IMPLEMENTATION!");
-}
-
-void Terrain_Generator::pop_quad() {
-  LOG_ERROR("NO IMPLEMENTATION!");
-}
-
-glm::vec2* Terrain_Generator::get(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-glm::vec2 Terrain_Generator::at(unsigned int index) {
-  LOG_ERROR("NO IMPLEMENTATION!");
-}
-
-const std::vector<glm::vec2>& Terrain_Generator::get_vector(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-unsigned int Terrain_Generator::get_points(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-void Terrain_Generator::clear(void) noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-void Terrain_Generator::push_quad_with_sprite(float size, glm::vec2 pos, glm::vec2 image_dimensions, glm::vec2 cell_size, glm::vec2 sprite_pos) {
-  LOG_ERROR("NO IMPLEMENTATION!");
-}
-
-glm::vec2* Terrain_Generator::get_with_sprite(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-}
-
-const std::vector<glm::vec2>& Terrain_Generator::get_vector_with_sprite(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-unsigned int Terrain_Generator::get_rendered_quads(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-void Terrain_Generator::gen_indices(int index) {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-const std::vector<unsigned int>& Terrain_Generator::get_vector_with_indices_buffer(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-unsigned int* Terrain_Generator::get_with_indices_buffer(void) const noexcept {
-  LOG_ERROR("NO IMPLEMENTATION!");
-};
-
-void CBaseMapReader::DumpMap(const char* szFileName) {
-    LOG_ERROR("NO IMPLEMENTATION!");
-    return;
-};
 
 void CBaseMapReader::ReadMap(const char* szFileName) {
   std::ifstream Handle(szFileName);
--- src/Terrain.hpp
+++ src/Terrain.hpp
@@ -29,43 +29,16 @@ private:
     int m_nLines = 0;
     std::vector<glm::vec2> m_QuadCoords;
     std::vector<Tile_t> m_Tiles;
-    float m_flTileSize = 4.0f;
+    float m_flTileSize;
     void setTilesOnMapRead(void);
 public:
+    CBaseMapReader() : m_flTileSize(4.0f) {};
     void                                    DumpMap(const char* szFileName);
     void                                    ReadMap(const char* szFileName);
     std::vector<glm::vec2>*                 GetBuffer(void) { return &m_QuadCoords; };
     std::vector<Tile_t>*                    GetTiles(void) { return &m_Tiles; };
     int                                     GetLineCount(void) const { return m_nLines; };
-    float                                   TileSize(void) const noexcept { return m_flTileSize; };
-};
-
-class Terrain_Generator {
-private:
-  std::vector<glm::vec2> data;
-  std::vector<glm::vec2> texture_pos;
-  std::vector<unsigned int> indices;
-  unsigned int points;
-public:
-  Terrain_Generator() : points(0) {};
-  void push_quad(float size, glm::vec2 pos);
-  void push_quad_with_sprite(float size, glm::vec2 pos, glm::vec2 image_dimensions, glm::vec2 cell_size, glm::vec2 sprite_pos);
-  void pop_quad();
-  void update_quad(unsigned int base, glm::vec2 pos, float size);
-  void clear(void) noexcept;
-  void gen_indices(int index);
-
-  glm::vec2 at(unsigned int index);
-  const std::vector<glm::vec2>& get_vector(void) const noexcept;
-  const std::vector<glm::vec2>& get_vector_with_sprite(void) const noexcept;
-  const std::vector<unsigned int>& get_vector_with_indices_buffer(void) const noexcept;
-  glm::vec2* get(void) const noexcept;
-  glm::vec2* get_with_sprite(void) const noexcept;
-  unsigned int* get_with_indices_buffer(void) const noexcept;
-  unsigned int get_points(void) const noexcept;
-  unsigned int get_rendered_quads(void) const noexcept;
-
-  ~Terrain_Generator() { this->clear(); };
+    float                                   GetTileSize(void) const noexcept { return m_flTileSize; };
 };
 
 class Terrain {
@@ -74,19 +47,13 @@ private:
     CShader m_Vertex, m_Fragment;
     CShaderProgram m_ShaderProgram;
     CBaseMapReader m_Reader;
-  Layout m_VertexArray;
-  Buffer m_VertexBuffer, indices_buffer, positions_buffer,
-      texture_positions_buffer;
-  Texture atlas;
-  std::vector<Vertex> m_MeshData;
-  std::vector<glm::vec2> tex_pos;
+    Layout m_VertexArray;
+    Buffer m_VertexBuffer, m_IndexBuffer;
+    Texture atlas;
+    std::vector<float> m_MeshData;
 
-  std::vector<glm::vec2> bacthed_terrain;
-  std::vector<unsigned int> indices;
-
-  float size = 16.0f;
-  AABB_Hitbox hitbox;
-  Terrain_Generator tg;
+    float size = 16.0f;
+    AABB_Hitbox hitbox;
 
 public:
   RENDERDOC_API_1_5_0 * rdoc_api = nullptr;
@@ -97,11 +64,3 @@ public:
   void test_tg(void);
   // void set_pos(glm::vec2 pos);
 };
-
-int write_point(int first_index, std::vector<glm::vec2> &collection,
-                glm::vec2 data);
-int write_quad(int first_index, std::vector<glm::vec2> &collection, float size,
-               glm::vec2 pos);
-std::vector<unsigned int> generateIndices(int quadCount);
-void set_texture_pos(Texture &texture, std::vector<glm::vec2> *tex_pos,
-                     glm::vec2 pos);
